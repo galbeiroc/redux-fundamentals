f309b198d2cc3e5a7ce6ae1837825c688b55b5f3
galbeiroc <alcresgu_1989@hotmail.com>
2021-12-29 12:36:03 -0500

    implement  dispacher and actions flux

---
+++ flux-app/src/flux/actions/colorAction.js
@@ -0,0 +1,12 @@
+import dispatcher from '../dispatcher';
+
+export const COLOR_APP_ACTIONS = {
+  CHANGE_COLOR: "ColorAppActions.ChangeColor",
+}
+
+export function changeColor(colorName) {
+  dispatcher.dispatch( payload: {
+    type: COLOR_APP_ACTIONS.CHANGE_COLOR,
+    value: colorName,
+  })
+}
