45b75a19fdf363ab36bc2fca3007d32e5df1647d
galbeiroc <alcresgu_1989@hotmail.com>
2021-12-29 13:18:40 -0500

    set store flux

--- flux-app/src/components/Box/Box.js
+++ flux-app/src/components/Box/Box.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react';
+import ColorStore from '../../flux/stores/colorStore';
+
 import './index.css';
 
 function Box() {
+  const [color, setColor] = useState(ColorStore.getActiveColor())
+  useEffect(() => {
+    ColorStore.on('storeUpdated', updateColor);
+
+    return () => {
+      ColorStore.removeListener('storeUpdated', updateColor)
+    }
+  }, []);
+
+  const updateColor = () => {
+    setColor(ColorStore.getActiveColor());
+  };
+
   return (
     <div className="box">
-      <div className="color-container"></div>
+      <div className="color-container" style={{ background: color }}></div>
     </div>
   )
 }
--- flux-app/src/components/Button/Button.js
+++ flux-app/src/components/Button/Button.js
@@ -1,10 +1,17 @@
+import * as ColorActions from '../../flux/actions/colorAction'
+
 import './index.css';
 
 function Button() {
+
+  const handleClik = (color) => {
+    ColorActions.changeColor(color);
+  };
+
   return (
     <div className="btn">
-      <button>Red</button>
-      <button>Green</button>
+      <button onClick={() => handleClik('#ff0000')}>Red</button>
+      <button onClick={() => handleClik('#00ff00')}>Green</button>
     </div>
   )
 }
