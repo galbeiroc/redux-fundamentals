df19badcc28db9261382a1e66650e5c3a381e6cf
galbeiroc <alcresgu_1989@hotmail.com>
2022-01-02 14:59:10 -0500

    create form by using Field

--- redux-app/src/pages/form/Form.js
+++ redux-app/src/pages/form/Form.js
@@ -7,10 +7,30 @@ const validate = values => {
   return errors;
 };
 
+const renderField = ({ label, input, type }) => (
+  <div>
+    <label className='label-cotrol'>{label}</label>
+    <div>
+      <input {...input} type={type} placeholder={label} className='form-control' />
+    </div>
+  </div>
+);
+
 let Form = () => {
   return (
     <div className='form'>
-      <h1>Form</h1>
+      <h3>Add new message</h3>
+      <form>
+        <div>
+          <Field name="subject" label="subject" type="text" component={renderField} />
+        </div>
+        <div>
+          <Field name="message" label="message" type="text" component={renderField} />
+        </div>
+        <div>
+          <input type="submit" value="send" className="send" />
+        </div>
+      </form>
     </div>
   )
 }
