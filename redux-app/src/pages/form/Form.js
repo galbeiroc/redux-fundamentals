afde3eed0e839094a4f6b53206d7b20ee730a1f0
galbeiroc <alcresgu_1989@hotmail.com>
2022-01-02 15:59:46 -0500

    add validations at form

--- redux-app/src/pages/form/Form.js
+++ redux-app/src/pages/form/Form.js
@@ -2,33 +2,52 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
 
+import './index.css';
+
 const validate = values => {
-  const errors = {};
+  const { subject, message } = values;
+  const errors = {}
+  
+  if (!subject) {
+    errors.subject = "Required";
+  } else if (subject?.length < 5) {
+    errors.subject = "Minimun be 5 characters or more";
+  }
+
+  if (!message) {
+    errors.message = "Required";
+  } else if (message?.length < 5) {
+    errors.message  = "Minimun be 5 characters or more";
+  }
+
   return errors;
 };
 
-const renderField = ({ label, input, type }) => (
+const renderField = ({ label, input, type, meta: { touched, error, warning } }) => (
   <div>
     <label className='label-cotrol'>{label}</label>
     <div>
       <input {...input} type={type} placeholder={label} className='form-control' />
+      {touched && ((error && 
+        <span className='text-danger'>{error}</span>) || (warning &&
+        <span className='text-warning'>{warning}</span>))}
     </div>
   </div>
 );
 
 let Form = () => {
   return (
-    <div className='form'>
+    <div className='content-form'>
       <h3>Add new message</h3>
       <form>
         <div>
-          <Field name="subject" label="subject" type="text" component={renderField} />
+          <Field name="subject" label="Subject" type="text" component={renderField} />
         </div>
         <div>
-          <Field name="message" label="message" type="text" component={renderField} />
+          <Field name="message" label="Message" type="text" component={renderField} />
         </div>
         <div>
-          <input type="submit" value="send" className="send" />
+          <input type="submit" value="Send" className="send" />
         </div>
       </form>
     </div>
@@ -36,7 +55,7 @@ let Form = () => {
 }
 
 Form = reduxForm({
-  form: "form",
+  form: "new",
   validate,
 })(Form)
 
